72bb53f1f7da2f2d7b604806d8662ff07ded6551
Slavik <hnatyshuns@gmail.com>
2018-05-30 14:00:43 +0300

    fixed

--- main.js
+++ main.js
@@ -12,40 +12,94 @@ canv.height = window.innerHeight;
 
 
 
-ctx.font = '16px Arial';
-ctx.fillStyle = '#72305f';
-ctx.fillText('Just use your finger or mouse to draw.', 50, 75);
+// ctx.font = '16px Arial';
+// ctx.fillStyle = '#72305f';
+// ctx.fillText('Just use your finger or mouse to draw.', 50, 75);
 
-ctx.fillStyle = '#e53c1e';
-ctx.fillText(' Click w to start.', 46, 100); 
-
-
-ctx.font = '14px Arial'
-ctx.fillStyle = '#000';
-ctx.fillText('Click s to save.', 50, 125);
-ctx.fillText(' Click r to replay what you saved.', 46, 150);
-ctx.fillText(' Click c to clear.', 46, 175);
+// ctx.fillStyle = '#e53c1e';
+// ctx.fillText(' Click w to start.', 46, 100); 
 
 
-document.addEventListener('keydown', (e) => {
+// ctx.font = '14px Arial'
+// ctx.fillStyle = '#000';
+// ctx.fillText('Click s to save.', 50, 125);
+// ctx.fillText(' Click r to replay what you saved.', 46, 150);
+// ctx.fillText(' Click c to clear.', 46, 175);
+
+
+// document.addEventListener('keydown', (e) => {
    
 
-    if (e.keyCode === 87){
-         //start
-        ctx.fillStyle = '#fff';
-        ctx.fillRect(0,0, canv.width, canv.height);
+//     if (e.keyCode === 87){
+//          //start
+//         ctx.fillStyle = '#fff';
+//         ctx.fillRect(0,0, canv.width, canv.height);
 
-        ctx.beginPath();
-        ctx.fillStyle = '#000';
+//         ctx.beginPath();
+//         ctx.fillStyle = '#000';
          
-    }
+//     }
 
-});
+// });
 
 
 
 
 // Code
+
+//basic functions 
+
+function save (){
+    localStorage.setItem('coords', JSON.stringify(coords));
+}
+
+
+
+function clear (){
+    ctx.fillStyle = '#fff';
+    ctx.fillRect(0,0, canv.width, canv.height);
+
+    ctx.beginPath();
+    ctx.fillStyle = '#000';
+}
+
+function replay (){
+ 
+   let timer = setInterval( function () {
+   
+    if(!coords.length){
+        clearInterval(timer);
+        ctx.beginPath();
+        return;
+    }
+
+    let crd = coords.shift();
+    let e = {
+        clientX: crd['0'],
+        clientY: crd['1']
+    };
+
+
+    ctx.lineTo(e.clientX, e.clientY)
+    ctx.lineWidth = 10 * 2;
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.arc(e.clientX, e.clientY, 10, 0 , Math.PI * 2);
+    
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.moveTo(e.clientX, e.clientY);
+
+
+
+   },30);
+
+}
+
+
+// basic functions for mouse stage
 
 canv.addEventListener('mousedown', () =>{
     isMouseDown = true;
@@ -60,55 +114,22 @@ canv.addEventListener('mouseup', () =>{
 
 });
 
-canv.addEventListener('mousemove', (e) =>{
+
+
+
+// brash 
+
+let brush = document.getElementById('brush');
+
+brush.addEventListener('click', () => {
+
+
+    canv.addEventListener('mousemove', (e) =>{
    
-    if( isMouseDown ){
-    
-    coords.push([e.clientX, e.clientY]);
-    ctx.lineTo(e.clientX, e.clientY)
-    ctx.lineWidth = 10 * 2;
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.arc(e.clientX, e.clientY, 10, 0 , Math.PI * 2);
-    ctx.fill();
-
-    ctx.beginPath();
-    ctx.moveTo(e.clientX, e.clientY);
-}
-   
-    function save (){
-        localStorage.setItem('coords', JSON.stringify(coords));
-    }
-
-
-
-    function clear (){
-        ctx.fillStyle = '#fff';
-        ctx.fillRect(0,0, canv.width, canv.height);
-
-        ctx.beginPath();
-        ctx.fillStyle = '#000';
-    }
-
-    function replay (){
-     
-       let timer = setInterval( function () {
-       
-        if(!coords.length){
-            clearInterval(timer);
-            ctx.beginPath();
-            return;
-        }
-
-        let crd = coords.shift();
-        let e = {
-            clientX: crd['0'],
-            clientY: crd['1']
-        };
-
-
-        ctx.lineTo(e.clientX, e.clientY)
+        if( isMouseDown ){
+        
+        coords.push([e.clientX, e.clientY]);
+        ctx.lineTo(e.clientX, e.clientY);
         ctx.lineWidth = 10 * 2;
         ctx.stroke();
     
@@ -118,17 +139,72 @@ canv.addEventListener('mousemove', (e) =>{
     
         ctx.beginPath();
         ctx.moveTo(e.clientX, e.clientY);
-
-
-
-       },30);
-
     }
+       
+});
 
 
-    document.addEventListener('keydown', (e) => {
+
+});
+
+
+
+// arc
+
+let arc = document.getElementById('arc');
+
+arc.addEventListener('click', () => {
    
+    canv.addEventListener('mousedown', (e) => {
+        coords.push([e.clientX, e.clientY]);
+        ctx.beginPath();
+        ctx.arc(e.clientX, e.clientY, 30, 0, Math.PI * 2);
+        ctx.fill(); 
+    });
 
+});
+
+
+// line 
+
+let line = document.getElementById('line');
+
+line.addEventListener('click', () =>{
+
+   
+   
+    canv.addEventListener('mousemove', (e) =>{
+   
+        if( isMouseDown ){
+        
+        coords.push([e.clientX, e.clientY]);
+        ctx.lineTo(e.clientX, e.clientY);
+     
+        ctx.stroke();
+    
+        // ctx.beginPath();
+        // ctx.arc(e.clientX, e.clientY, 5, 0 , Math.PI * 2);
+        // ctx.fill();
+    
+        ctx.beginPath();
+        ctx.moveTo(e.clientX, e.clientY);
+    }
+       
+});
+
+});
+
+
+
+
+
+
+
+
+
+document.addEventListener('keydown', (e) => {
+       
+    
     if (e.keyCode === 83){
          //save
          save();
@@ -151,4 +227,5 @@ canv.addEventListener('mousemove', (e) =>{
     }
 });
 
-});
+
+
